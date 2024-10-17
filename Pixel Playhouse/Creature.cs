5e41f1685180661217c2e0c6d17c8ccd34aed30a
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-17 11:48:51 +0200

    Added Creature class, and also added the properties for Name, HitPoint, MaxItemsCarry, and Items. Also implemented the methods Hit, RecieveHit, and Loot.

---
+++ Pixel Playhouse/Creature.cs
@@ -0,0 +1,29 @@
+﻿namespace Pixel_Playhouse;
+
+public abstract class Creature
+{
+    protected string? Name { get; set; }
+    protected int HitPoint { get; set; }
+    protected int MaxItemsCarry { get; set; }
+    protected List<WorldObject> Items { get; set; } = new List<WorldObject>();
+
+    protected int Hit(AttackItem attackItem)
+    {
+        return attackItem.Hit;
+    }
+
+    protected void RecieveHit(int hit)
+    {
+        HitPoint -= hit;
+    }
+
+    protected void Loot(WorldObject worldObject)
+    {
+        if (Items.Count >= MaxItemsCarry)
+        {
+            throw new Exception("Cannot carry more items");
+        }
+
+        Items.Add(worldObject);
+    }
+}
