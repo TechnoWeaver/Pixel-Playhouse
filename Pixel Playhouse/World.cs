7861e7b088ae4a83615d2be03aad9e8604adb774
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-17 12:56:47 +0200

    Refactored to a base clase for the template pattern design, which can be used as a base class for a game world. Changed the property GameWorld to public. Added lists for containing world objects and creatures. Added an InitializeGameWorld method, and also added abstract methods SetWorldSize, AddWorldObjects, and AddCreatures, which must be implemented. Finally also added documentation.

--- Pixel Playhouse/World.cs
+++ Pixel Playhouse/World.cs
@@ -1,19 +1,66 @@
 ﻿namespace Pixel_Playhouse;
 
-public class World
+/// <summary>
+/// Represents an abstract based class for the world.
+/// </summary>
+public abstract class World
 {
+    /// <summary>
+    /// Gets or sets the maximum world coordinate X.
+    /// </summary>
     public int MaxX { get; set; }
-    public int MaxY { get; set; }
-    
-    protected int[,] GameWorld { get; set; }
 
+    /// <summary>
+    /// Gets or sets the maximum world coordinate Y.
+    /// </summary>
+    public int MaxY { get; set; }
+
+    /// <summary>
+    /// Represents a matrix of the game world.
+    /// </summary>
+    public int[,] GameWorld { get; set; }
+
+    /// <summary>
+    /// A list containing all the world objects.
+    /// </summary>
+    public List<WorldObject> WorldObjects { get; set; } = new List<WorldObject>();
+
+    /// <summary>
+    /// A list containing all the creatures.
+    /// </summary>
+    public List<Creature> Creatures { get; set; } = new List<Creature>();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="World"/> class.
+    /// </summary>
     protected World()
     {
-        if (MaxX <= 0 || MaxY <= 0)
-        {
-            throw new Exception("Invalid world size");
-        }
-
-        GameWorld = new int[--MaxX, --MaxY];
+        GameWorld = SetWorldSize();
+        InitializeGameWorld();
     }
+
+    /// <summary>
+    /// Initializes the game world.
+    /// </summary>
+    protected void InitializeGameWorld()
+    {
+        AddWorldObjects();
+        AddCreatures();
+    }
+
+    /// <summary>
+    /// A method which must be implemented to set the world size.
+    /// </summary>
+    /// <returns>The world size as a matrix.</returns>
+    public abstract int[,] SetWorldSize();
+
+    /// <summary>
+    /// A method which must be implemented to add world objects.
+    /// </summary>
+    public abstract void AddWorldObjects();
+
+    /// <summary>
+    /// A method which must be implemented to add creatures.
+    /// </summary>
+    public abstract void AddCreatures();
 }
