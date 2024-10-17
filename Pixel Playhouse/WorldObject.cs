78a1740206d755e93f1b489b5094c07f195b75c5
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-17 11:50:38 +0200

    Added WorldObject class with the properties Name, Lootable, and Removeable.

---
+++ Pixel Playhouse/WorldObject.cs
@@ -0,0 +1,8 @@
+﻿namespace Pixel_Playhouse;
+
+public abstract class WorldObject
+{
+    public string? Name { get; set; }
+    public bool Lootable { get; set; }
+    public bool Removeable { get; set; }
+}
