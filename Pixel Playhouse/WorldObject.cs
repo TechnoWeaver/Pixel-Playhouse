0e8462e46bb52feacdeda45c117333299211d7df
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-17 13:09:35 +0200

    Refactored to abstract base class for template design pattern. Added enums for IsLootable and IsRemoveable to easily set it. Added a constructor, which setups the WorldObject. Added a InitializeWorldObject method. Added three methods SetName, SetLootableState, and SetRemoveableState, which must be implemented. Finally also added documentation.

--- Pixel Playhouse/WorldObject.cs
+++ Pixel Playhouse/WorldObject.cs
@@ -1,8 +1,73 @@
 ﻿namespace Pixel_Playhouse;
 
-public class WorldObject
+/// <summary>
+/// An enumeration representing the lootable state of a world object.
+/// </summary>
+public enum IsLootable
 {
+    Yes,
+    No
+}
+
+/// <summary>
+/// An enumeration representing the removeable state of a world object.
+/// </summary>
+public enum IsRemoveable
+{
+    Yes,
+    No
+}
+
+/// <summary>
+/// An abstract class representing a world object.
+/// </summary>
+public abstract class WorldObject
+{
+    /// <summary>
+    /// A string representing the name of the world object.
+    /// </summary>
     public string? Name { get; set; }
+
+    /// <summary>
+    /// A boolean representing the lootable state of the world object.
+    /// 
     public bool Lootable { get; set; }
+
+    /// <summary>
+    /// A boolean representing the removeable state of the world object.
+    /// 
     public bool Removeable { get; set; }
+
+
+    /// <summary>
+    /// A constructor for the world object.
+    protected WorldObject()
+    {
+        InitializeWorldObject();
+    }
+
+    /// <summary>
+    /// A method to initialize the world object.
+    /// 
+    protected void InitializeWorldObject()
+    {
+        SetName();
+        SetLootableState();
+        SetRemoveableState();
+    }
+
+    /// <summary>
+    /// A method which must be implemented to set the name of the world object.
+    /// 
+    public abstract void SetName();
+
+    /// <summary>
+    /// A method which must be implemented to set the lootable state of the world object.
+    /// 
+    public abstract void SetLootableState();
+
+    /// <summary>
+    /// A method which must be implemented to set the removeable state of the world object.
+    /// 
+    public abstract void SetRemoveableState();
 }
