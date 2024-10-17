94ae04e38dd662667a7e6a0e0d034045366b00bb
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-17 13:20:26 +0200

    Refactored to abstract base class for template design pattern. Added constructor. Added method to Initialize the AttackItem. Added three methods SetHit and SetRange, which must be implemented.

--- Pixel Playhouse/AttackItem.cs
+++ Pixel Playhouse/AttackItem.cs
@@ -1,7 +1,43 @@
 ﻿namespace Pixel_Playhouse;
 
-public class AttackItem: WorldObject
+public abstract class AttackItem : WorldObject
 {
+    /// <summary>
+    /// Gets or sets the hit points of the attack item.
+    /// </summary>
     public int Hit { get; set; }
+
+    /// <summary>
+    /// Gets or sets the range of the attack item.
+    /// 
     public int Range { get; set; }
+
+
+    /// <summary>
+    /// A constructor for the attack item.
+    /// 
+    protected AttackItem()
+    {
+        InitializeWorldObject();
+    }
+
+    /// <summary>
+    /// A method to initialize the world object.
+    /// 
+    protected new void InitializeWorldObject()
+    {
+        SetName();
+        SetHit();
+        SetRange();
+        SetLootableState();
+        SetRemoveableState();
+    }
+
+    /// <summary>
+    /// A method which must be implemented to set the hit points of the attack item.
+    public abstract void SetHit();
+
+    /// <summary>
+    /// A method which must be implemented to set the range of the attack item.
+    public abstract void SetRange();
 }
