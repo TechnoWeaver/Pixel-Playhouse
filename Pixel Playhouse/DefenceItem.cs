cd007f9a343371e2b43b4c76374d01005b27d3a3
Gabriel H. Kierkegaard <gabkierk@proton.me>
2024-10-18 16:25:12 +0200

    Refactored into an abstract class for use with the template design pattern. Implemented variables and properties needed. Also made abstract methods, which must be implemented by the game itself.

--- Pixel Playhouse/DefenceItem.cs
+++ Pixel Playhouse/DefenceItem.cs
@@ -1,6 +1,40 @@
 ﻿namespace Pixel_Playhouse;
 
-public class DefenceItem: WorldObject
+/// <summary>
+/// An abstract class representing a defence item.
+/// </summary>
+public abstract class DefenceItem: WorldObject
 {
-    public int ReduceHitPoint { get; set; }
+    /// <summary>
+    /// A variable representing the hit points that the attack item is reduced by.
+    protected int defensePoints;
+
+    /// <summary>
+    /// Gets the defense points of the item.
+    /// 
+    public int DefensePoints => defensePoints;
+
+    /// <summary>
+    /// A constructor for the defence item.
+    /// 
+    protected DefenceItem()
+    {
+        InitializeWorldObject();
+    }
+
+    /// <summary>
+    /// A method to initialize the DefenceItem.
+    /// 
+    protected new void InitializeWorldObject()
+    {
+        SetName();
+        SetDefensePoints();
+        SetLootableState();
+        SetRemoveableState();
+    }
+
+    /// <summary>
+    /// A method which must be implemented to set the defense points of the item.
+    /// 
+    public abstract void SetDefensePoints();
 }
